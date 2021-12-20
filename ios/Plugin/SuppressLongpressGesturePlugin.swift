3a7969a945313382745c25af87d8bf69ab50a64d
Nikita Shchetko <corrosion.by@gmail.com>
2021-12-20 16:30:45 +0300

    bugfix: service reactivation caused sometime strange behaviour of service;

--- ios/Plugin/SuppressLongpressGesturePlugin.swift
+++ ios/Plugin/SuppressLongpressGesturePlugin.swift
@@ -8,34 +8,43 @@ import Capacitor
 @objc(SuppressLongpressGesturePlugin)
 public class SuppressLongpressGesturePlugin: CAPPlugin {
     private let implementation = SuppressLongpressGesture()
+    var longPressRecognizerActivated: Bool = false
 
     @objc func activateService(_ call: CAPPluginCall) {
-        print("activateService!")
-        let webView = self.bridge?.webView
-        // recognize long-press gesture to prevent haptic feedback (selection start, standard behavior in Safari)
-        let recognizeLongPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongpressGesture))
-        recognizeLongPressGesture.minimumPressDuration = 0.45
-        recognizeLongPressGesture.allowableMovement = 100.0
-        // add this in main thread
-        DispatchQueue.main.async {
-            // add gesture recognizer from above
-            webView?.addGestureRecognizer(recognizeLongPressGesture)
-            // resolve plugin call
-            call.resolve()
+
+        if longPressRecognizerActivated == false {
+            print("activateService!")
+            let webView = self.bridge?.webView
+            // recognize long-press gesture to prevent haptic feedback (selection start, standard behavior in Safari)
+            let recognizeLongPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongpressGesture))
+            recognizeLongPressGesture.minimumPressDuration = 0.45
+            recognizeLongPressGesture.allowableMovement = 100.0
+            // add this in main thread
+            DispatchQueue.main.async {
+                // add gesture recognizer from above
+                webView?.addGestureRecognizer(recognizeLongPressGesture)
+                // resolve plugin call
+                call.resolve()
+            }
+        longPressRecognizerActivated = true;
         }
     }
 
     @objc func deactivateService(_ call: CAPPluginCall) {
-    print("DEactivate Service!")
-    let webView = self.bridge?.webView
+    
+    if longPressRecognizerActivated == true {
+        print("DEactivate Service!")
+        let webView = self.bridge?.webView
 
-    // add this in main thread
-    DispatchQueue.main.async {
-        for recognizer in webView?.gestureRecognizers ?? [] {
-            webView?.removeGestureRecognizer(recognizer)
+        // add this in main thread
+        DispatchQueue.main.async {
+            for recognizer in webView?.gestureRecognizers ?? [] {
+                webView?.removeGestureRecognizer(recognizer)
+            }
+            // resolve plugin call
+            call.resolve()
         }
-        // resolve plugin call
-        call.resolve()
+        longPressRecognizerActivated = false
     }
 }
 
